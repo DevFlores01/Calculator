18c4c15260e257759fb8c5618052440f3f3db43c
DevFlores01 <floresalainreuben01@gmail.com>
2024-04-23 19:07:25 +0800

    Added Shape

---
+++ app/src/main/java/dev/flores/calculator/CalculatorViewModel.kt
@@ -0,0 +1,46 @@
+package dev.flores.calculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.flores.calculator.ui.theme.CalculatorTheme
+
+class CalculatorViewModel : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CalculatorTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting6("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting6(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview6() {
+    CalculatorTheme {
+        Greeting6("Android")
+    }
+}
